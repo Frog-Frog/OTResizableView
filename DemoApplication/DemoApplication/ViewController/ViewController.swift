0cf83ae811d0c891d576c08b3de8ffbcf0988e4c
Tomosuke Okada <orenoti@gmail.com>
2017-09-22 23:23:52 +0900

    Update DemoApp.

--- DemoApplication/DemoApplication/ViewController/ViewController.swift
+++ DemoApplication/DemoApplication/ViewController/ViewController.swift
@@ -12,48 +12,57 @@ import OTResizableView
 
 class ViewController: UIViewController,OTResizableViewDelegate {
 
+    var resizableView = OTResizableView(contentView: UIView())
+    
+    @IBOutlet weak var mainView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let yourView = UIView(frame: CGRect(x: 40, y: 40, width: 200, height: 300))
+        let yourView = UIView(frame: CGRect(x: 70, y: 70, width: 200, height: 300))
         yourView.backgroundColor = UIColor.blue
         
-        let resizableView = OTResizableView(contentView: yourView)
+        resizableView = OTResizableView(contentView: yourView)
         resizableView.delegate = self;
         
-        self.view.addSubview(resizableView)
+        mainView.addSubview(resizableView)
     }
 
     
-    override func didReceiveMemoryWarning()
-    {
+    override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
     
+    @IBAction func tappedChangeAspectButton(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        if sender.isSelected {
+            resizableView.keepAspectEnabled = true
+        } else {
+            resizableView.keepAspectEnabled = false
+            resizableView.minimumWidth = 100
+            resizableView.minimumHeight = 100
+        }
+    }
     
-    func tapBegin(resizableView: OTResizableView)
-    {
+    func tapBegin(_ resizableView: OTResizableView) {
         resizableView.resizeEnabled = resizableView.resizeEnabled ? false : true
         
         print("tapBegin:\(NSStringFromCGRect(resizableView.frame))")
     }
     
     
-    func tapChanged(resizableView: OTResizableView)
-    {
+    func tapChanged(_ resizableView: OTResizableView) {
         print("changeNow:\(NSStringFromCGRect(resizableView.frame))")
     }
     
     
-    func tapMoved(resizableView: OTResizableView)
-    {
+    func tapMoved(_ resizableView: OTResizableView) {
         print("tapMoved:\(NSStringFromCGRect(resizableView.frame))")
     }
     
     
-    func tapEnd(resizableView: OTResizableView)
-    {
+    func tapEnd(_ resizableView: OTResizableView) {
         print("tapEnd:\(NSStringFromCGRect(resizableView.frame))")
     }
 }
